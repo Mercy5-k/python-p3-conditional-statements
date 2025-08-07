1fa2b899415b4835fadf95672b33106b47feea39
Mercy5-k <kiqsmercy@gmail.com>
2025-08-07 17:53:38 +0300

    Completed lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
 
 from ast import Return
+from urllib import response
 
 
 def admin_login(username, password):
     # your code here
      if (username == "admin" or username == "ADMIN") and password == "12345":
-        return "Access granted"
-     else: 
-        return "Access denied"
+         return "Access granted"
+     else:
+         return "Access denied"
+     pass
 
 def hows_the_weather(temperature):
     # your code here
-    if temperature <40:
-        return  "It's brisk out there!"
-    elif temperature <=65:
-        return  "It's a little chilly out there!"
-    elif temperature > 85:
-        return "It's too dang hot out there!"
+    response = ""
+    if temperature < 40:
+        response = "brisk"
+    elif temperature >=40 and temperature <=65:
+        response = "a little chilly"
+    elif temperature >85:
+        response = "too dang hot"
     else:
-        return "It's perfect out there!"
+        response = "perfect"
+    return f"It's {response} out there!"
+    pass
     
 def fizzbuzz(num):
     # your code here
@@ -31,6 +36,7 @@ def fizzbuzz(num):
         return "Buzz"
     else:
         return num
+    pass
 
 def calculator(operation, num1, num2):
     # your code here
@@ -43,5 +49,5 @@ def calculator(operation, num1, num2):
     elif operation == "/":
         return num1 / num2
     else:
-         print("Invalid operation!")
-    return None
+        print ("Invalid operation!")
+    pass
